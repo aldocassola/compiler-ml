6c5c4296e37da954dcb36a1443df6bb8a8168119
Aldo <aldocassola@users.noreply.github.com>
2020-03-18 22:07:52 -0400

    Update entrypoint.sh

--- .github/actions/chap4-tests/entrypoint.sh
+++ .github/actions/chap4-tests/entrypoint.sh
@@ -9,7 +9,7 @@ if ! [ -d testcases ]; then
     (curl -s ${tiger_tar_url} | tar xf - tiger/testcases) || exit 1
 fi
 
-allfiles="testcases/*.tig"
+allfiles="tiger/testcases/*.tig"
 
 smlout=$( (echo CM.make \"sources.cm\"\;
 for f in "$allfiles"; do \

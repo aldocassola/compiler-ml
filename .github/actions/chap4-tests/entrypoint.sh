bdb74dd4ca9352bf12384648ff181d0650fee321
Aldo Cassola <aldocassola@gmail.com>
2020-03-26 21:11:29 -0400

    try single output

--- .github/actions/chap4-tests/entrypoint.sh
+++ .github/actions/chap4-tests/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 tiger_tar_url=https://www.cs.princeton.edu/~appel/modern/ml/tiger.tar
+expected_fail=testcases/test49.tig
 
 cd ${GITHUB_WORKSPACE}/chap4
-echo pwd is $(pwd)
 
 if ! [ -d testcases ]; then
     (curl -s ${tiger_tar_url} | tar xf - tiger/testcases) || exit 1
@@ -13,13 +13,15 @@ fi
 
 allfiles="tiger/testcases/*.tig"
 
-smlout=$( (echo CM.make \"sources.cm\"\;
-for f in $allfiles; do \
-    echo Parse.parse \"$f\"\;
-done ) | sml 2> error.log)
+smlout=$((echo CM.make \"sources.cm\"\;
+    echo map Parse.parse \[
+    for f in $allfiles;
+    do
+        echo -n \"$f\",
+    done | sed 's/,$//'; echo \]\;) | sml 2> error.log)
 
 status=$?
 
 echo ::set-output name=compile-out::"$smlout"
-echo ::set-output name=compile-err::"$(cat error.log)"
+#echo ::set-output name=compile-err::"$(grep -v ${expected_fail} error.log)"
 exit $status
